110f44ac33f25545caf86406a7d1d53fb87edee9
Willy Woitas <willy.woitas@neofonie.de>
2021-12-13 13:14:22 +0100

    oida #7

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,29 +5,34 @@ import Client from 'ssh2-sftp-client';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
-const secrets = yargs(hideBin(process.argv)).argv;
+//const secrets = yargs(hideBin(process.argv)).argv;
 const sftp = new Client();
 const src = './public';
 
-console.log(secrets)
+let privateKey = '';
 
-const privateKey = '-----BEGIN RSA PRIVATE KEY-----\n' + secrets.privateKey.replace('|', '\n') + '\n-----END RSA PRIVATE KEY-----';
-//privateKey: fs.readFileSync(secrets.privateKey),
-console.log(privateKey)
+// console.log(privateKey);
+// privateKey = fs.readFileSync(secrets.privateKey);
+// console.log('fs', privateKey);
+privateKey = '-----BEGIN RSA PRIVATE KEY-----\nProc-Type: 4,ENCRYPTED\nDEK-Info: ' + secrets.privateKey.replace(/\|/g, '\n') + '\n-----END RSA PRIVATE KEY-----\n';
+//console.log('bufferraw', privateKey);
+privateKey = Buffer.from(privateKey, 'utf-8');
+//console.log('buffer', privateKey);
 
-sftp.connect({
-    host: secrets.host,
-    port: secrets.port,
-    username: secrets.username,
-    passphrase: secrets.passphrase,
-    privateKey: Buffer.from(privateKey, 'utf-8'),
-
-})
-    .then(() => sftp.uploadDir(src, secrets.destination))
-    .then(msg => {
-        console.log(msg);
-        return sftp.end();
+if (true) {
+    sftp.connect({
+        host: secrets.host,
+        port: secrets.port,
+        username: secrets.username,
+        passphrase: secrets.passphrase,
+        privateKey,
     })
-    .catch(err => {
-        console.log(err, 'catch error');
-    });
+        .then(() => sftp.uploadDir(src, secrets.destination))
+        .then(msg => {
+            console.log('Upload dome:', msg);
+            return sftp.end();
+        })
+        .catch(err => {
+            console.log(err, 'catch error');
+        });
+}
