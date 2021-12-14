ec2f1c9dc9f7785762ec219e19c5cf4305991144
Willy Woitas <willy.woitas@neofonie.de>
2021-12-14 23:42:20 +0100

    add login secret

--- scripts/commit-changes.sh
+++ scripts/commit-changes.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 echo "commit change"
-
-#git pull
-
-#npm run fetch
-
-#git add *
-
+# get latest repo changes
+git pull
+# fetch graphql
+npm run fetch
+# add all changes
+git add .*
+# use json for commit message
 # ../data/api-changes.last-cursor.json
-#value=$(<../data/api-changes.last-cursor.json)
-#git commit -m "Add latest $value"
+value=$(<../data/api-changes.last-cursor.json)
+git commit -m "Add latest $value"
+# update repo
+# https://github.com/dutscher/bbx.git
+login=$(<./bbx.secret)
+git push https://$(login)@github.com/dutscher/bbx.git --all
 
-#git push
+#git reset --hard
