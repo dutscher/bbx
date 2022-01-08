d6b3a2197f8c7b34a169a195e6b3d6a9931d245e
Willy Woitas <willy.woitas@neofonie.de>
2022-01-09 00:49:16 +0100

    update welcome

--- src/comps/Welcome.svelte
+++ src/comps/Welcome.svelte
@@ -39,28 +39,33 @@
     BBX Watcher
 
     <div class="fb-like" data-href="https://www.facebook.com/bbxwatcher" data-width="170px" data-layout="button_count"
-          data-action="like" data-size="small" data-share="true"></div>
+         data-action="like" data-size="small" data-share="true"></div>
 </h1>
 {#if !isOnline}
     <span class="warning">
-        Deine Internet Verbindung ist weg aber du kannst den Watcher weiterhin nutzen.<br />
+        Deine Internet Verbindung ist weg aber du kannst den Watcher weiterhin nutzen.<br/>
         <strong>Letzter Stand:</strong> {lastCursor[0] && lastCursor[0].split('|')[1]}
     </span>
 {/if}
 <div class="welcome{isVisible ? ' show' : ''}">
     <div class="welcome__content">
         <p>
-            Dies ist ein von der Community gebauter Kompakter Bluebrixx only Produkte Katalog.<br/>
-            Aufgrund des vielen klickens und langer Ladezeiten im BB eigenen Online-Shop haben wir das hier entwickelt.
+            Dies ist ein Kompakter Bluebrixx only Produkte Katalog.<br/>
+            Aufgrund des vielen klickens und langen Ladezeiten im BB eigenen Online-Shop, haben wir das hier entwickelt.
             <b>Wir sind nicht Bluebrixx</b>.
         <p>
-            Wähle einen der <b>{tags} Tags</b> (Jedes hat einen Deeplink in der URL) aus, suche nach einem von
+        <p>
+            Wähle einen der <b>{tags} Tags</b> aus, suche nach einem von
             <b>{products} Bluebrixx Produkten</b> oder
-            wähle einen Verfügbarkeit Status, außerdem gibt es noch einen Status-Verlauf in der Detailansicht.<br/>
-            Zu jedem Produkt gibts noch die passende Anleitung (Zum herrunterladen müsst ihr aber bei BB eingeloggt
-            sein)<br/>
-            Aber Obacht: es gibt noch 3 Überraschungen ;)<br/>
-            Viel Spaß
+            wähle einen Verfügbarkeit Status.
+        </p>
+        <p>
+            Außerdem gibt es noch einen Status-Verlauf pro Produkt in der Detailansicht.<br/>
+            Obendrauf noch die passenden Anleitungen verfügbar (Zum herunterladen müsst ihr aber bei BB eingeloggt sein)<br/>
+        </p>
+        <p>
+            Aber Obacht: es gibt noch 3 Eastereggs, Könnt Ihr sie finden?<br/><br/>
+            Viel Spaß damit und Feedback bitte via Facebook oder E-Mail
         </p>
     </div>
     <div class="welcome__other">
