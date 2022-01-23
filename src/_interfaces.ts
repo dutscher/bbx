2c8a8111cd66e450de4dac1900c3f6dcd7278ede
Willy Woitas <willy.woitas@neofonie.de>
2022-01-23 23:03:43 +0100

    add new ids and comment

--- src/_interfaces.ts
+++ src/_interfaces.ts
@@ -1,4 +1,9 @@
-export const AFF_LINK = '?aff=wrhjxrxb'; // noppensteinnews
+// Die ausgehenden Links "Zum Shop" von jedem Produkt sind mit einen Affiliate Link versehen.
+// Mit einem Klick und anschließendem Kauf unterstütz du dieses Projekt.
+// Wir selbst düfen noch kein Partner von Bluebrixx sein daher bekommt temporär noppensteinnews die Provision.
+// Die ist der Fall weil ich zu Gast im Podcast über dieses Projekt sprechen durfte
+export const AFF_LINK = '?aff=wrhjxrxb';
+// ID Stuff
 export const ID_MANHATTAN = 17;
 export const STR_MANHATTAN = 'Manhattan';
 export const ID_NETHERLAND = 57;
@@ -15,7 +20,7 @@ export const ID_STATE_COMING_SOON = 1;
 export const ID_STATE_UNAVAILABLE = 2;
 export const ID_STATE_ANNOUNCEMENT = 3;
 export const IDS_SPECIAL_TAGS = [ID_PARTS, ID_MANHATTAN, ID_MOVIE, ID_NETHERLAND, ID_BURG_BLAUSTEIN];
-
+// Localstorage
 export const lsKeyWelcome = 'welcomeVisible';
 export const lsKeyChanges = 'changesVisible';
 export const lsKeyFilter = 'filterVisible';
@@ -24,7 +29,7 @@ export interface Tag {
     id: number,
     name: string,
 }
-
+// external data
 export const UNLOADED = 'unloaded';
 export const LOADING = 'loading';
 export const LOADED = 'loaded';
