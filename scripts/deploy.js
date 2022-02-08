6766bf9893c538eaeec9cd128bf0dce0ae66cbd8
Willy Woitas <willy.woitas@neofonie.de>
2022-02-08 12:08:36 +0100

    prettier

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,8 @@
 //import secrets from './ftp.secret.json';
 import { Buffer } from 'buffer';
 import Client from 'ssh2-sftp-client';
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 const secrets = yargs(hideBin(process.argv)).argv;
 const sftp = new Client();
@@ -14,25 +14,29 @@ let privateKey = '';
 // console.log(privateKey);
 // privateKey = fs.readFileSync(secrets.privateKey);
 // console.log('fs', privateKey);
-privateKey = '-----BEGIN RSA PRIVATE KEY-----\nProc-Type: 4,ENCRYPTED\nDEK-Info: ' + secrets.privateKey.replace(/\|/g, '\n') + '\n-----END RSA PRIVATE KEY-----\n';
+privateKey =
+  '-----BEGIN RSA PRIVATE KEY-----\nProc-Type: 4,ENCRYPTED\nDEK-Info: ' +
+  secrets.privateKey.replace(/\|/g, '\n') +
+  '\n-----END RSA PRIVATE KEY-----\n';
 //console.log('bufferraw', privateKey);
 privateKey = Buffer.from(privateKey, 'utf-8');
 //console.log('buffer', privateKey);
 
 if (true) {
-    sftp.connect({
-        host: secrets.host,
-        port: secrets.port,
-        username: secrets.username,
-        passphrase: secrets.passphrase,
-        privateKey,
+  sftp
+    .connect({
+      host: secrets.host,
+      port: secrets.port,
+      username: secrets.username,
+      passphrase: secrets.passphrase,
+      privateKey,
     })
-        .then(() => sftp.uploadDir(src, secrets.destination))
-        .then(msg => {
-            console.log('Upload done:', msg);
-            return sftp.end();
-        })
-        .catch(err => {
-            console.log(err, 'catch error');
-        });
+    .then(() => sftp.uploadDir(src, secrets.destination))
+    .then(msg => {
+      console.log('Upload done:', msg);
+      return sftp.end();
+    })
+    .catch(err => {
+      console.log(err, 'catch error');
+    });
 }
--- scripts/fetch-stats.js
+++ scripts/fetch-stats.js
@@ -8,156 +8,148 @@ const params = process.argv;
 
 // self parsed
 let parsedDataToday = {
-    items: products,
-    images: {}
+  items: products,
+  images: {},
 };
 let allTimeChanges = {};
 
-const parseImageExtension = async (product) => {
-    const defaultExt = 'png'
-    const image = `${product.id}_1.${defaultExt}`;
-    // "/img/items/100/100300/300/100300_2.jpg"
-    // //www.bluebrixx.com/img/items/104/104701/150/104701_1.png
-    const url = `https://www.bluebrixx.com/img/items/${('' + product.id).substr(0, 3)}/${product.id}/150/${image}`;
-    const res = await fetch(url);
+const parseImageExtension = async product => {
+  const defaultExt = 'png';
+  const image = `${product.id}_1.${defaultExt}`;
+  // "/img/items/100/100300/300/100300_2.jpg"
+  // //www.bluebrixx.com/img/items/104/104701/150/104701_1.png
+  const url = `https://www.bluebrixx.com/img/items/${('' + product.id).substr(0, 3)}/${product.id}/150/${image}`;
+  const res = await fetch(url);
 
-    if (res.status === 404) {
-        console.log('jpg for', url);
-        return 0; // jpg
-    }
+  if (res.status === 404) {
+    console.log('jpg for', url);
+    return 0; // jpg
+  }
 
-    // imageExtensions
-    return -1; // png
-}
+  // imageExtensions
+  return -1; // png
+};
 
-const mergeChangesWithDB = async (allTimeChanges) => {
-    await Promise.all( // due fetch for image is that async
-        Object.entries(allTimeChanges).map(async (productChange) => {
-            const productId = parseInt(productChange[0]);
-            const changes = productChange[1];
-            let isNewProduct = false;
+const mergeChangesWithDB = async allTimeChanges => {
+  await Promise.all(
+    // due fetch for image is that async
+    Object.entries(allTimeChanges).map(async productChange => {
+      const productId = parseInt(productChange[0]);
+      const changes = productChange[1];
+      let isNewProduct = false;
 
-            let product = parsedDataToday.items.find((product) => product.id === productId);
-            // new product
-            if (!product) {
-                product = {};
-                isNewProduct = true;
-            }
+      let product = parsedDataToday.items.find(product => product.id === productId);
+      // new product
+      if (!product) {
+        product = {};
+        isNewProduct = true;
+      }
 
-            product.id = productId;
-            product.title = changes.title;
-            product.state = changes.state;
-            if (changes.parts !== null) {
-                product.parts = changes.parts;
-            }
-            product.price = changes.price;
-            // TODO: parse cats for existing products
-            //product.cats = changes.cats;
-            //product.tags = changes.tags;
-            // history
-            product.history = {
-                ...product.history,
-                ...changes.history,
-            }
+      product.id = productId;
+      product.title = changes.title;
+      product.state = changes.state;
+      if (changes.parts !== null) {
+        product.parts = changes.parts;
+      }
+      product.price = changes.price;
+      // TODO: parse cats for existing products
+      //product.cats = changes.cats;
+      //product.tags = changes.tags;
+      // history
+      product.history = {
+        ...product.history,
+        ...changes.history,
+      };
 
-            cleanUpHistoryChange(product);
+      cleanUpHistoryChange(product);
 
-            if (false && product.id === 100090) {
-                console.log(product, changes)
-            }
+      if (false && product.id === 100090) {
+        console.log(product, changes);
+      }
 
-            if (isNewProduct) {
-                product.cats = changes.cats;
-                product.tags = changes.tags;
-                // parse correct image extension
-                const imageExtension = await parseImageExtension(product);
-                if (imageExtension !== -1) {
-                    product.imageExt = imageExtension;
-                }
+      if (isNewProduct) {
+        product.cats = changes.cats;
+        product.tags = changes.tags;
+        // parse correct image extension
+        const imageExtension = await parseImageExtension(product);
+        if (imageExtension !== -1) {
+          product.imageExt = imageExtension;
+        }
 
-                parsedDataToday.items.push(product);
-            }
-        })
-    );
-}
+        parsedDataToday.items.push(product);
+      }
+    })
+  );
+};
 
 (async () => {
-    let startDate = moment(new Date());
-    allTimeChanges = await fetchChanges(true);
+  let startDate = moment(new Date());
+  allTimeChanges = await fetchChanges(true);
 
-    startDate = printTime('fetchChanges', startDate); // 1394ms
+  startDate = printTime('fetchChanges', startDate); // 1394ms
 
-    // readd existing history, which was done in svelte via ajax
-    products.map(product => {
-        product.history = allProductHistory[product.id];
+  // readd existing history, which was done in svelte via ajax
+  products.map(product => {
+    product.history = allProductHistory[product.id];
+  });
+
+  await mergeChangesWithDB(allTimeChanges);
+
+  startDate = printTime('mergeChangesWithDB', startDate); // 289ms
+
+  if (params.includes('--parse-pages')) {
+    await parsePages(allTimeChanges);
+  }
+
+  if (params.includes('--parse-pages-n-parts')) {
+    await parsePagesNParts(allTimeChanges);
+  }
+
+  // write items
+  let orderedProducts = parsedDataToday.items
+    .sort((a, b) => {
+      if (a.id < b.id) {
+        return -1;
+      }
+      if (a.id > b.id) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(product => {
+      return convertToReduce(product);
     });
 
-    await mergeChangesWithDB(allTimeChanges);
+  // exclude history
+  const productHistory = {};
+  orderedProducts = orderedProducts.map(product => {
+    productHistory[product.id] = product.hi;
+    delete product.hi;
+    return product;
+  });
 
-    startDate = printTime('mergeChangesWithDB', startDate); // 289ms
-
-    if (params.includes('--parse-pages')) {
-        await parsePages(allTimeChanges);
+  if (params.includes('--create-compare')) {
+    // write images files
+    if (Object.keys(parsedDataToday.images).length > 0) {
+      await handleCache(
+        './data/api/',
+        `images.compare.json`,
+        () => JSON.stringify(parsedDataToday.images, null, 2),
+        true
+      );
     }
+    // write compare file
+    await handleCache('./data/', `all-products.compare.json`, () => JSON.stringify(orderedProducts, null, 2), true);
 
-    if (params.includes('--parse-pages-n-parts')) {
-        await parsePagesNParts(allTimeChanges);
-    }
-
-    // write items
-    let orderedProducts = parsedDataToday.items.sort((a, b) => {
-        if (a.id < b.id) {
-            return -1;
-        }
-        if (a.id > b.id) {
-            return 1;
-        }
-        return 0;
-    }).map((product) => {
-        return convertToReduce(product);
-    });
-
-    // exclude history
-    const productHistory = {};
-    orderedProducts = orderedProducts.map((product) => {
-        productHistory[product.id] = product.hi;
-        delete product.hi;
-        return product;
-    });
-
-    if (params.includes('--create-compare')) {
-        // write images files
-        if (Object.keys(parsedDataToday.images).length > 0) {
-            await handleCache(
-                './data/api/',
-                `images.compare.json`,
-                () => JSON.stringify(parsedDataToday.images, null, 2),
-                true);
-        }
-        // write compare file
-        await handleCache(
-            './data/',
-            `all-products.compare.json`,
-            () => JSON.stringify(orderedProducts, null, 2),
-            true);
-
-        await handleCache(
-            './data/',
-            `all-products-history.compare.json`,
-            () => JSON.stringify(productHistory, null, 2),
-            true);
-    } else {
-        await handleCache(
-            './data/',
-            `all-products.json`,
-            () => JSON.stringify(orderedProducts, null, 2),
-            true);
-        await handleCache(
-            './data/',
-            `all-products-history.json`,
-            () => JSON.stringify(productHistory, null, 2),
-            true);
-    }
-    printTime('all done', startDate); // 50ms
+    await handleCache(
+      './data/',
+      `all-products-history.compare.json`,
+      () => JSON.stringify(productHistory, null, 2),
+      true
+    );
+  } else {
+    await handleCache('./data/', `all-products.json`, () => JSON.stringify(orderedProducts, null, 2), true);
+    await handleCache('./data/', `all-products-history.json`, () => JSON.stringify(productHistory, null, 2), true);
+  }
+  printTime('all done', startDate); // 50ms
 })();
-
