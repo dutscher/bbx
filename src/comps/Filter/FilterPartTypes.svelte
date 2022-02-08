c67152949e44b66cc3becc5452d28d247cc0a344
Willy Woitas <willy.woitas@neofonie.de>
2022-02-08 12:12:10 +0100

    prettier

--- src/comps/Filter/FilterPartTypes.svelte
+++ src/comps/Filter/FilterPartTypes.svelte
@@ -1,100 +1,102 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { storedActiveSelection, storedPartTypes, storedProducts, storedFilteredProducts } from '../../stores';
-    import { getUrlParam, setUrlParams, titleMatch } from '../../utils';
-    import { ID_PARTS } from "../../_interfaces";
+  import { onMount } from 'svelte';
+  import { storedActiveSelection, storedPartTypes, storedProducts, storedFilteredProducts } from '../../stores';
+  import { getUrlParam, setUrlParams, titleMatch } from '../../utils';
+  import { ID_PARTS } from '../../_interfaces';
 
-    export let activePartTypeIds: any = [];
-    export let partTypes: any;
-    export let products: any;
-    export let filteredProducts: any = [];
+  export let activePartTypeIds: any = [];
+  export let partTypes: any;
+  export let products: any;
+  export let filteredProducts: any = [];
 
-    const urlParam = 'partTypes';
-    const getUrlParams = () => {
-        const queryTags = getUrlParam(urlParam).split(',');
-        partTypes.map(part => {
-            queryTags.map(queryTag => {
-                if (part.seoName === queryTag) {
-                    clickItem(part);
-                }
-            });
-        });
-    }
-
-    const clickItem = (item, withUrlUpdate?) => {
-        if (item.count === 0) return;
-
-        storedActiveSelection.update(store => {
-            if (!(urlParam in store)) {
-                store[urlParam] = [];
-            }
-            if (!store[urlParam].includes(item.id)) {
-                store[urlParam].push(item.id);
-            } else {
-                store[urlParam] = store[urlParam].filter(itemId => itemId !== item.id);
-            }
-
-            if (withUrlUpdate) {
-                setUrlParams(
-                    urlParam,
-                    partTypes
-                        .filter(part => store[urlParam].includes(part.id))
-                        .map((part) => part.seoName));
-                store.reason = 'part-type-clicked';
-            } else {
-                store.reason = 'url-parsed';
-            }
-            return store;
-        });
-    }
-
-    storedPartTypes.subscribe(store => partTypes = store);
-    storedProducts.subscribe(store => products = store);
-    storedFilteredProducts.subscribe(store => filteredProducts = store);
-
-    onMount(() => {
-        getUrlParams();
+  const urlParam = 'partTypes';
+  const getUrlParams = () => {
+    const queryTags = getUrlParam(urlParam).split(',');
+    partTypes.map(part => {
+      queryTags.map(queryTag => {
+        if (part.seoName === queryTag) {
+          clickItem(part);
+        }
+      });
     });
+  };
 
-    function sortItems() {
-        let sortedData = [];
-        // get count of products
-        sortedData = partTypes.map(part => {
-            part.count =
-                (filteredProducts && filteredProducts.withFilter.length > 0 ? filteredProducts.withFilter : products)
-                    .filter(product => product.tags.includes(ID_PARTS))
-                    .filter(product =>
-                        titleMatch(part, product) > 0
-                    ).length;
-            return part;
-        })
-            // sort state
-            .sort((a, b) => {
-                if (a.label < b.label) {
-                    return -1;
-                }
-                if (a.label > b.label) {
-                    return 1;
-                }
-                return 0;
-            });
-        return sortedData;
-    }
+  const clickItem = (item, withUrlUpdate?) => {
+    if (item.count === 0) return;
 
-    $: sortedItems = sortItems(filteredProducts);
+    storedActiveSelection.update(store => {
+      if (!(urlParam in store)) {
+        store[urlParam] = [];
+      }
+      if (!store[urlParam].includes(item.id)) {
+        store[urlParam].push(item.id);
+      } else {
+        store[urlParam] = store[urlParam].filter(itemId => itemId !== item.id);
+      }
+
+      if (withUrlUpdate) {
+        setUrlParams(
+          urlParam,
+          partTypes.filter(part => store[urlParam].includes(part.id)).map(part => part.seoName)
+        );
+        store.reason = 'part-type-clicked';
+      } else {
+        store.reason = 'url-parsed';
+      }
+      return store;
+    });
+  };
+
+  storedPartTypes.subscribe(store => (partTypes = store));
+  storedProducts.subscribe(store => (products = store));
+  storedFilteredProducts.subscribe(store => (filteredProducts = store));
+
+  onMount(() => {
+    getUrlParams();
+  });
+
+  function sortItems() {
+    let sortedData = [];
+    // get count of products
+    sortedData = partTypes
+      .map(part => {
+        part.count = (
+          filteredProducts && filteredProducts.withFilter.length > 0 ? filteredProducts.withFilter : products
+        )
+          .filter(product => product.tags.includes(ID_PARTS))
+          .filter(product => titleMatch(part, product) > 0).length;
+        return part;
+      })
+      // sort state
+      .sort((a, b) => {
+        if (a.label < b.label) {
+          return -1;
+        }
+        if (a.label > b.label) {
+          return 1;
+        }
+        return 0;
+      });
+    return sortedData;
+  }
+
+  $: sortedItems = sortItems(filteredProducts);
 </script>
+
 <div class="flex">
-    <h4>Typen</h4>
-    <div class="flex flex--wrap bl">
-        {#each sortedItems as part (part.id)}
-            <div class="part{activePartTypeIds.includes(part.id) ? ' active': ''}{part.count === 0 ? ' disabled': ''}"
-                 data-id={part.id}
-                 on:click={() => clickItem(part, true)}
-                 title="{part.name} ({part.count})">
-                {part.de}
-            </div>
-        {/each}
-    </div>
+  <h4>Typen</h4>
+  <div class="flex flex--wrap bl">
+    {#each sortedItems as part (part.id)}
+      <div
+        class="part{activePartTypeIds.includes(part.id) ? ' active' : ''}{part.count === 0 ? ' disabled' : ''}"
+        data-id={part.id}
+        on:click={() => clickItem(part, true)}
+        title="{part.name} ({part.count})"
+      >
+        {part.de}
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style lang="scss">
@@ -116,7 +118,9 @@
       font-size: ms(-2);
     }
 
-    &:focus, &:active, &.active {
+    &:focus,
+    &:active,
+    &.active {
       background: $color-primary-darker;
       color: $color-white;
     }
