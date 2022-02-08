1aea9e8154d729f4521dbf6ead99b620a2588cd0
Willy Woitas <willy.woitas@neofonie.de>
2022-02-08 12:07:33 +0100

    prettier

--- scripts/src/clean-utils.js
+++ scripts/src/clean-utils.js
@@ -1,69 +1,68 @@
 // 2021-08-26T05:41:14+00:00
 // 26.08.2021 07:41 = GMT+2
-export const getHRDate = (dateStr) => {
-    const now = !!dateStr ? new Date(dateStr) : new Date();
-    const year = now.getFullYear();
-    const month = ((now.getMonth() + 1) + '').padStart(2, '00');
-    const day = ((now.getDate()) + '').padStart(2, '00');
-    const hour = ((now.getHours()) + '').padStart(2, '00');
-    const minute = ((now.getMinutes()) + '').padStart(2, '00');
-    return `${day}.${month}.${year} ${hour}:${minute}`;
-}
+export const getHRDate = dateStr => {
+  const now = !!dateStr ? new Date(dateStr) : new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1 + '').padStart(2, '00');
+  const day = (now.getDate() + '').padStart(2, '00');
+  const hour = (now.getHours() + '').padStart(2, '00');
+  const minute = (now.getMinutes() + '').padStart(2, '00');
+  return `${day}.${month}.${year} ${hour}:${minute}`;
+};
 
-export const isDST = (d) => {
-    let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
-    let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
-    return Math.max(jan, jul) !== d.getTimezoneOffset();
-}
+export const isDST = d => {
+  let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
+  let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
+  return Math.max(jan, jul) !== d.getTimezoneOffset();
+};
 
 // 26.08.2021 07:41
 // 2021-08-26T07:41:00+02:00
-export const getDateTime = (hrDate) => {
-    const isSummertime = isDST(new Date());
-    const date = hrDate.split(' ');
-    const dmy = date[0].split('.');
-    return `${dmy[2]}-${dmy[1]}-${dmy[0]}T${date[1] || '00:00'}:00+0${!isSummertime ? 2 : 1}:00`;
-}
+export const getDateTime = hrDate => {
+  const isSummertime = isDST(new Date());
+  const date = hrDate.split(' ');
+  const dmy = date[0].split('.');
+  return `${dmy[2]}-${dmy[1]}-${dmy[0]}T${date[1] || '00:00'}:00+0${!isSummertime ? 2 : 1}:00`;
+};
 
 export const mergeArrays = (...arrays) => {
-    let jointArray = []
+  let jointArray = [];
 
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array || []]
-    })
-    const uniqueArray = jointArray.reduce((newArray, item) => {
-        if (newArray.includes(item)) {
-            return newArray
-        } else {
-            return [...newArray, item]
-        }
-    }, [])
-    return uniqueArray
-}
+  arrays.forEach(array => {
+    jointArray = [...jointArray, ...(array || [])];
+  });
+  const uniqueArray = jointArray.reduce((newArray, item) => {
+    if (newArray.includes(item)) {
+      return newArray;
+    } else {
+      return [...newArray, item];
+    }
+  }, []);
+  return uniqueArray;
+};
 
 export const mergeTags = (existingTags, newTags, additionalTags) => {
-    return mergeArrays(
-        existingTags,
-        newTags,
-        (additionalTags || []),
-    ).sort(sortTags);
-}
+  return mergeArrays(existingTags, newTags, additionalTags || []).sort(sortTags);
+};
 
 export const sortTags = (a, b) => a - b;
 
-export const cleanUpHistoryChange = (product) => {
-    // sort timestamps
-    const sortObject = o => Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {})
-    product.history = sortObject(product.history);
+export const cleanUpHistoryChange = product => {
+  // sort timestamps
+  const sortObject = o =>
+    Object.keys(o)
+      .sort()
+      .reduce((r, k) => ((r[k] = o[k]), r), {});
+  product.history = sortObject(product.history);
 
-    // clean up double states
-    let lastState = -1;
-    let newHistory = {};
-    for (const [timestamp, state] of Object.entries(product.history)) {
-        if (lastState !== state) {
-            lastState = state
-            newHistory[timestamp] = state;
-        }
+  // clean up double states
+  let lastState = -1;
+  let newHistory = {};
+  for (const [timestamp, state] of Object.entries(product.history)) {
+    if (lastState !== state) {
+      lastState = state;
+      newHistory[timestamp] = state;
     }
-    product.history = newHistory;
-}
+  }
+  product.history = newHistory;
+};
--- src/stores/colors.ts
+++ src/stores/colors.ts
@@ -2,26 +2,26 @@ import { writable } from 'svelte/store';
 import colorsJSON from '../../data/bricklink-colors.json';
 import bricklinkColors from '../../data/bricklink-hex.json';
 // sort colors
-const sortedColors = colorsJSON.map((color) => {
-    const name = Array.isArray(color) ? color[0].toString() : color;
-    const bricklinkName = name
-        .replace('Flesh', 'Nougat')
-        .replace('Medium Dark Nougat', 'Medium Nougat')
-        .replace('Pearl Light Gray, 95 Flat Silver', 'Flat Silver');
-    const lastElement = Array.isArray(color) ? color[color.length - 1] : null;
-    let hex, id;
-    if ((bricklinkName in bricklinkColors)) {
-        [hex, id] = bricklinkColors[bricklinkName].split('|');
-    }
-    return {
-        name,
-        seoName: name.toLowerCase().replace(/ /g, '-'),
-        de: Array.isArray(color) ? color.length > 0 ? color[1] : color[0] : '',
-        matcher: Array.isArray(color) ? color : [color],
-        ignores: Array.isArray(lastElement) ? color[color.length - 1] : [],
-        hex,
-        id,
-    }
-})
+const sortedColors = colorsJSON.map(color => {
+  const name = Array.isArray(color) ? color[0].toString() : color;
+  const bricklinkName = name
+    .replace('Flesh', 'Nougat')
+    .replace('Medium Dark Nougat', 'Medium Nougat')
+    .replace('Pearl Light Gray, 95 Flat Silver', 'Flat Silver');
+  const lastElement = Array.isArray(color) ? color[color.length - 1] : null;
+  let hex, id;
+  if (bricklinkName in bricklinkColors) {
+    [hex, id] = bricklinkColors[bricklinkName].split('|');
+  }
+  return {
+    name,
+    seoName: name.toLowerCase().replace(/ /g, '-'),
+    de: Array.isArray(color) ? (color.length > 0 ? color[1] : color[0]) : '',
+    matcher: Array.isArray(color) ? color : [color],
+    ignores: Array.isArray(lastElement) ? color[color.length - 1] : [],
+    hex,
+    id,
+  };
+});
 export const storedColors = writable([]);
 storedColors.set(sortedColors);
