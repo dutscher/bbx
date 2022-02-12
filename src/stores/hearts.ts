b2ce5162d3f28f4bf6a440f090ea3cb4dcc4bd07
Willy Woitas <willy.woitas@neofonie.de>
2022-02-12 21:37:35 +0100

    fix hearts

--- src/stores/hearts.ts
+++ src/stores/hearts.ts
@@ -6,12 +6,14 @@ const defaultList = 'default';
 let lsStore = localStore.get(lsKey);
 
 // fallback do migration / [103217] -> { default: {t:'Merkliste',i:[103217]} }
-if (Array.isArray(lsStore)) {
-  localStore.set(lsKey, JSON.stringify({ [defaultList]: { t: 'Merkliste', i: lsStore } }));
-  lsStore = localStore.get(lsKey);
-  // fallback do migration / {"default":[104000,104185,105030,104953]} -> { default:{t:'Merkliste',i:[103217]} }
-} else if ('default' in lsStore && Array.isArray(lsStore.default)) {
-  localStore.set(lsKey, JSON.stringify({ [defaultList]: { t: 'Merkliste', i: lsStore.default } }));
-  lsStore = localStore.get(lsKey);
+if (lsStore) {
+  if (Array.isArray(lsStore)) {
+    localStore.set(lsKey, JSON.stringify({ [defaultList]: { t: 'Merkliste', i: lsStore } }));
+    lsStore = localStore.get(lsKey);
+    // fallback do migration / {"default":[104000,104185,105030,104953]} -> { default:{t:'Merkliste',i:[103217]} }
+  } else if ('default' in lsStore && Array.isArray(lsStore.default)) {
+    localStore.set(lsKey, JSON.stringify({ [defaultList]: { t: 'Merkliste', i: lsStore.default } }));
+    lsStore = localStore.get(lsKey);
+  }
 }
 export const storedHearts = writable(lsStore || { [defaultList]: { t: 'Merkliste', i: [] } });
