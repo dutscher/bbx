3fefa5241151a1258931021ca63a8a833dffdefa
Willy Woitas <willy.woitas@neofonie.de>
2023-02-04 16:47:02 +0100

    fix lasst bugs

--- src/comps/Features/Heartlist/HeartlistShare.svelte
+++ src/comps/Features/Heartlist/HeartlistShare.svelte
@@ -93,7 +93,7 @@
         />
         <label for="uuid">CloudID</label>
         <span class="helper">
-          {#if !uuid}Drücke auf <b>"Generiere CloudID"</b> oder gib hier eine vorhandene 5-stellige <b>CloudID</b> ein{/if}
+          {#if !uuid}<b>"Generiere CloudID"</b> oder gib hier eine 5-stellige <b>CloudID</b> ein{/if}
         </span>
       </div>
     </div>
