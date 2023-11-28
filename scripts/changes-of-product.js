e719dd5946a80863ca13e113bf64c01a186558c4
Willy Woitas <willy.woitas@neofonie.de>
2023-11-28 14:05:02 +0100

    pew pew update categories

--- scripts/changes-of-product.js
+++ scripts/changes-of-product.js
@@ -1,20 +1,28 @@
 import states from '../data/states.json' assert { type: 'json' };
+import prompts from 'prompts';
 
-const productId = 107297;
-const apiUrl = `https://api.bbx.watch/bapi/product/detail/${productId}`;
-
-const json = await fetch(apiUrl).then(res => res.json());
-
-const actualState = json.status;
-const lastChanges = json.lastChanges;
-const history = {};
-
-lastChanges.map(change => {
-  const date = new Date(change.datetime);
-  // INITIAL_FULLDATA, INITIAL
-  const state = change.status.includes('INITIAL') ? actualState : change.status;
-  history[date.getTime() / 1000] = states.api.indexOf(state);
+const response = await prompts({
+  type: 'number',
+  name: 'productId',
+  message: 'Gebe eine ProduktID ein? (Bsp.: 102870)',
 });
+if (!!response.productId && response.productId.toString().length === 6) {
+  const apiUrl = `https://api.bbx.watch/bapi/product/detail/${response.productId}`;
+  console.log({ apiUrl });
+  const json = await fetch(apiUrl).then(res => res.json());
 
-console.log(JSON.stringify({ [productId]: history, json }, null, 2));
-console.log({ apiUrl });
+  const actualState = json.status;
+  const lastChanges = json.lastChanges;
+  const history = {};
+
+  console.log(json);
+
+  lastChanges.map(change => {
+    const date = new Date(change.datetime);
+    // INITIAL_FULLDATA, INITIAL
+    const state = change.status.includes('INITIAL') ? actualState : change.status;
+    history[date.getTime() / 1000] = states.api.indexOf(state);
+  });
+
+  console.log(JSON.stringify({ [response.productId]: history, json }, null, 2));
+}
